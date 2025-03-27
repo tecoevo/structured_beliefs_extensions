5847157a24700f6d5753f1d88bb6c579c4892eb6
Sagnik634 <hsagnik2004@gmail.com>
2025-03-27 17:26:41 +0530

    figuresource push

--- FigureSources/Prisoner's dilemma plot.py
+++ FigureSources/Prisoner's dilemma plot.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 R, S, T, P = 3, 0, 5, 1  
-def replicator_dynamics(x):
-    f_C = x * R + (1 - x) * S  
-    f_D = x * T + (1 - x) * P  
-    return x * (1 - x) * (f_C - f_D)
+def f_C(x):  
+    return R * x + S * (1 - x)  
+def f_D(x):  
+    return T * x + P * (1 - x) s
+def replicator_eq(x):
+    f_avg = x * f_C(x) + (1 - x) * f_D(x)  
+    return x * (f_C(x) - f_avg)  
 x_values = np.linspace(0, 1, 100)
-dx_dt_values = replicator_eq=(x_values)
+dx_dt_values = replicator_eq(x_values)
 plt.figure(figsize=(8, 5))
 plt.plot(x_values, dx_dt_values, label=r'$\frac{dx}{dt} = x(1-x)(f_C - f_D)$', color='b')
 plt.axhline(0, color='black', linestyle='--', linewidth=1)  
